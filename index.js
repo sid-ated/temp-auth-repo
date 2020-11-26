ac86989b126c5725fad9d03b128088690e551a58
Siddhartha Shukla <siddhartha.shukla.cer16@itbhu.ac.in>
2020-11-26 22:13:02 +0530

    test10

--- index.js
+++ index.js
@@ -22,48 +22,43 @@ function verifyToken(token){
   return  jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ?  decode : err)
 }
 
-serverAuth.get('/temp', (req, res) => {
+serverAuth.post('/temp', (req, res) => {
 //const url = "http://localhost:3000/users";
 const url = "https://aichemist-server.herokuapp.com/users";
 fetch(url)
 .then((resp) => resp.json()) 
 .then(function(data) {
-  res.status(200).jsonp("working")
+  var idx = data.findIndex(user => user.username === req.body.username && user.password === req.body.password)
+  res.status(200).jsonp(idx)
 })
 .catch(err => console.log(err))
-
 });
 
-// Check if the user exists in database\
-
-async function isAuthenticated({username, password}){
-	//const url = "http://localhost:3000/users";
-	const url = "https://aichemist-server.herokuapp.com/users";
-	fetch(url)
-	.then((resp) => resp.json()) 
-	.then(function(data, answer) {
-	  //return data.findIndex(user => user.username === username && user.password === password) !== -1
-	})
-	.catch(err => console.log(err))
-}
 
 // Register New User
 serverAuth.post('/register', (req, res) => {
   console.log("register endpoint called; request body:");
   console.log(req.body);
   const {username, password} = req.body;
+  /*
   if(isAuthenticated({username, password}) === true) {
     res.statusCode = 401;
     res.setHeader('Content-Type', 'application/json');
     res.json({success: false, status: "This user already exist", err: 401});
-  }
-	  
+  }*/
+  //const url = "http://localhost:3000/users";
 	const url = "https://aichemist-server.herokuapp.com/users";
-	//let url = "http://localhost:3000/users";
 	fetch(url)
 	.then((resp) => resp.json()) 
 	.then(function(data) {
-	  var last_item_id = data[data.length-1].id;
+	  var idx = data.findIndex(user => user.username === req.body.username && user.password === req.body.password)
+	  if (idx!==-1) {
+	    res.statusCode = 401;
+	    res.setHeader('Content-Type', 'application/json');
+	    res.json({success: false, status: "This user already exist", err: 401});
+	  }
+	  else{
+	     var last_item_id = data[data.length-1].id;
 	  //data.push({id: last_item_id + 1, username: username, password: password, isAdmin: false});
 		    fetch(url, { 
 		      
@@ -73,8 +68,8 @@ serverAuth.post('/register', (req, res) => {
 		    // Adding body or contents to send 
 		    body: JSON.stringify({
 		     id: last_item_id +1,
-		     username: username,
-		     password: password,
+		     username: req.body.username,
+		     password: req.body.password,
 		     isAdmin: false
 		    }
 		    ), 
@@ -85,35 +80,39 @@ serverAuth.post('/register', (req, res) => {
 		    } 
 		}) 
 		.then(response => response.json()) 
-
-		// Displaying results to console 
-		.then(json => console.log(json));
+		const access_token = createToken({username, password})
+		  res.statusCode = 200;
+		  res.setHeader('Content-Type', 'application/json');
+		  res.json({success: true, status: 'Registration Successful! You are loged in.', token: access_token });
+	   }
 	})
 	.catch(err => console.log(err))
-
-	// Create token for new user
-	  const access_token = createToken({username, password})
-	  res.statusCode = 200;
-	  res.setHeader('Content-Type', 'application/json');
-	  res.json({success: true, status: 'Registration Successful! You are loged in.', token: access_token });
 })
 
 // Login to one of the users from ./users.json
 serverAuth.post('/login', (req, res) => {
   console.log("login endpoint called; request body:");
   console.log(req.body);
-  
-  const {username, password} = req.body;
- 
-  if (isAuthenticated({username, password}) === false) {
-    res.statusCode = 401;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({success: false, status: 'Login Unsuccessful', err: 401});
-  }
-  const access_token = createToken({username, password})
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'Login Successful', token: access_token });
+  //const url = "http://localhost:3000/users";
+	const url = "https://aichemist-server.herokuapp.com/users";
+	fetch(url)
+	.then((resp) => resp.json()) 
+	.then(function(data) {
+	  var idx = data.findIndex(user => user.username === req.body.username && user.password === req.body.password)
+	  if (idx===-1) {
+	    res.statusCode = 401;
+	    res.setHeader('Content-Type', 'application/json');
+	    res.json({success: false, status: 'Login Unsuccessful', err: 401});
+	  }
+	  else{
+	   const {username, password} = req.body
+	   const access_token = createToken({username, password})
+		  res.statusCode = 200;
+		  res.setHeader('Content-Type', 'application/json');
+		  res.json({success: true, status: 'Login Successful', token: access_token });
+	  }
+	})
+	.catch(err => console.log(err))
 
 })
 
